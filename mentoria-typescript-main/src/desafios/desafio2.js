b17ecb7fba6cbe3310c708f9c2b07fbd7e91e259
leopdani <51030652+leopdani@users.noreply.github.com>
2022-08-02 15:29:38 -0300

    Add files via upload

---
+++ mentoria-typescript-main/src/desafios/desafio2.js
@@ -0,0 +1,23 @@
+// Como podemos melhorar o esse código usando TS? 
+
+let pessoa1 = {};
+pessoa1.nome = "Decio";
+pessoa1.idade = 64;
+pessoa1.profissao = "Engenheiro"
+
+let pessoa2 = {}
+pessoa2.nome = "Bruna";
+pessoa2.idade = 26;
+pessoa2.profissao = "Médica";
+
+let pessoa3 = {
+    nome: "Leonardo",
+    idade: "27",
+    profissao: "Engenheiro"
+};
+
+let pessoa4 = {
+    nome:  "Cecilia",
+    idade:  "63",
+    profissao:  "Médica"
+}
